4db5fff82ce84d108daf50917be16737ff7d22e9
xNorthen <tiberifederico1234@gmail.com>
2022-09-18 12:15:51 +0200

    aggiuntaDROP

--- team1.sql
+++ team1.sql
@@ -13,6 +13,8 @@ PRIMARY KEY (slot, posizione)
 -- I prezzi hanno al massimo 2 cifre intere e 2 decimali
 -- e non possono essere numeri negativi
 
+DROP TABLE IF EXISTS prodotti;
+
 CREATE TABLE IF NOT EXISTS prodotti(
 
 nome VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -22,6 +24,7 @@ costo INT UNSIGNED
 );
 
 
+DROP TABLE IF EXISTS merendine;
 CREATE TABLE  merendine(
 
 scadenza VARCHAR(50),
@@ -35,6 +38,9 @@ FOREIGN KEY (nome) REFERENCES prodotti(nome)
 
 --  nomi e cognomi delle persone
 --  non possono essere nulli o stringhe vuote
+
+DROP TABLE IF EXISTS persona;
+
 CREATE TABLE IF NOT EXISTS persone(
 
 -- controllo se nome e cognome sono stringhe vuote :)

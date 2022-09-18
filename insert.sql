65b937519942bdc37e26545b46a725b583509d8d
Lorenzo Gilli <lorenzogilli.lg@gmail.com>
2022-09-18 12:12:36 +0200

    robah

--- insert.sql
+++ insert.sql
@@ -1,1 +1,21 @@
-ciao 
+
+INSERT INTO posto(slot,posizione) VALUES (1,1);
+INSERT INTO posto(slot,posizione) VALUES (1,2);
+INSERT INTO posto(slot,posizione) VALUES (1,3);
+INSERT INTO posto(slot,posizione) VALUES (1,4);
+INSERT INTO posto(slot,posizione) VALUES (1,5);
+INSERT INTO posto(slot,posizione) VALUES (2,1);
+INSERT INTO posto(slot,posizione) VALUES (2,2);
+INSERT INTO posto(slot,posizione) VALUES (2,3);
+INSERT INTO posto(slot,posizione) VALUES (2,4);
+INSERT INTO posto(slot,posizione) VALUES (2,5);
+INSERT INTO posto(slot,posizione) VALUES (3,1);
+INSERT INTO posto(slot,posizione) VALUES (3,2);
+INSERT INTO posto(slot,posizione) VALUES (3,3);
+INSERT INTO posto(slot,posizione) VALUES (3,4);
+INSERT INTO posto(slot,posizione) VALUES (3,5);
+INSERT INTO posto(slot,posizione) VALUES (4,1);
+INSERT INTO posto(slot,posizione) VALUES (4,2);
+INSERT INTO posto(slot,posizione) VALUES (4,3);
+INSERT INTO posto(slot,posizione) VALUES (4,4);
+INSERT INTO posto(slot,posizione) VALUES (4,5);

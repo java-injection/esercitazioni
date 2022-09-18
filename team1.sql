426dc9c5f7f5a110d8d85a612c37c87f398ae86f
xNorthen <tiberifederico1234@gmail.com>
2022-09-18 11:55:08 +0200

    Lorenzo Sei Inutile

--- team1.sql
+++ team1.sql
@@ -1,2 +1,38 @@
-ciao
-show databases;
+
+CREATE database distributore;
+
+DROP TABLE IF EXISTS posti;
+
+CREATE TABLE IF NOT EXISTS posti (
+
+slot INT UNSIGNED PRIMARY KEY,
+posizione INT UNSIGNED PRIMARY KEY
+
+);
+
+-- I prezzi hanno al massimo 2 cifre intere e 2 decimali
+-- e non possono essere numeri negativi
+
+CREATE TABLE IF NOT EXISTS prodotti(
+
+nome VARCHAR(50) PRIMARY KEY NOT NULL,
+tipo VARCHAR(50) NOT NULL,
+costo INT UNSIGNED
+
+);
+
+
+--  nomi e cognomi delle persone
+--  non possono essere nulli o stringhe vuote
+CREATE TABLE IF NOT EXISTS persone(
+
+-- controllo se nome e cognome sono stringhe vuote :)
+CONSTRAINT stringhe_svuote CHECK(nome != "" AND cognome != ""),
+
+CF VARCHAR(16) NOT NULL PRIMARY KEY,
+
+-- nome e cognome non possono essere stringhe vuote :)
+nome VARCHAR(50) NOT NULL, 
+cognome VARCHAR(50) NOT NULL
+
+);

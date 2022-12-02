bcaa2a0130b3abe9140d17b789a7aabfbade3bed
davide <david.cascella.5@gmail.com>
2022-12-02 17:15:35 +0100

    fixes

--- esercitazione4/esercitazione4.sql
+++ esercitazione4/esercitazione4.sql
@@ -6,9 +6,46 @@ use esercitazione4;
 -- ***************************************************************************************************************************
 -- *								FASE 1   UTILITY FUNCTION
 -- ***************************************************************************************************************************
+SET GLOBAL log_bin_trust_function_creators = 1;
 
 -- scrivere una funzione che dati 2 intervalli interi restituisca un valore randomico compreso tra i 2 intervalli.
+CREATE USER 'account'@'localhost' IDENTIFIED BY 'Massimobrutto' ;
+GRANT EXECUTE
+ON esercitazione4.*
+TO 'account'@'localhost';
+drop function if exists randnum;
 
+delimiter $$
+CREATE DEFINER='account'@'localhost' function RandNum ( 
+ minimo int, massimo int
+)
+returns  int 
+not deterministic
+
+SQL SECURITY DEFINER 
+begin
+if(minimo is null or massimo is null)
+then 
+	SET @exception = 'PESCE METTIMI DEI NUMERI';
+    signal sqlstate '45000'
+	SET MESSAGE_TEXT = @exception,
+	MYSQL_ERRNO= 1062;
+END IF;
+if(minimo = massimo)
+then 
+	set @mammt =1;
+	SET @exception = 'PESCE sono identici';
+    signal sqlstate '01000'
+	SET MESSAGE_TEXT = @exception,
+	MYSQL_ERRNO= 22023;
+END IF;
+
+return FLOOR(minimo + RAND()*(massimo - minimo + 1)) ; 
+end $$
+delimiter ;
+select @mammt;
+SELECT randnum(0,10);
+show warnings;
 -- TIP: questa query restituisce un numero randomico intero tra 5 e 10:
 -- SELECT FLOOR(5 + RAND()*(10 - 5 + 1)) AS Random_Number;
 
@@ -16,11 +53,323 @@ use esercitazione4;
 -- la funzione deve generare un warning se i valori dell'intervallo sono uguali 
 -- la funzione deve appoggiarsi ad un definer ad hoc e deve operare in definer-context
 
+DROP TABLE IF EXISTS sensori;
 
+CREATE TABLE IF NOT EXISTS sensori(
+  id_sensor INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(20) NOT NULL,
+  brand VARCHAR(50) NOT NULL,
+  UNIQUE(nome, brand)
+);
+
+INSERT INTO sensori VALUES (1,'EL-100','SAMSUNG');
+INSERT INTO sensori VALUES (2,'DOOR-007','OBS');
+
+
+
+DROP TABLE IF EXISTS data_sensori;
+
+CREATE TABLE IF NOT EXISTS data_sensori(
+     timestamp DATETIME NOT NULL,
+     data JSON NOT NULL,
+     id_sensor INT UNSIGNED NOT NULL,
+     FOREIGN KEY (id_sensor) REFERENCES sensori(id_sensor)
+);
+
+
+                      --            ABBSADBASBBSA
+delimiter $$
+CREATE DEFINER='account'@'localhost' function RandNum ( 
+ minimo int, massimo int
+)
+returns  int 
+not deterministic
+
+SQL SECURITY DEFINER 
+begin
+if(minimo is null or massimo is null)
+then 
+	SET @exception = 'PESCE METTIMI DEI NUMERI';
+    signal sqlstate '45000'
+	SET MESSAGE_TEXT = @exception,
+	MYSQL_ERRNO= 1062;
+END IF;
+if(minimo = massimo)
+then 
+	set @mammt =1;
+	SET @exception = 'PESCE sono identici';
+    signal sqlstate '01000'
+	SET MESSAGE_TEXT = @exception,
+	MYSQL_ERRNO= 22023;
+END IF;
+
+
+
+
+
+drop function if exists inserimento;
+delimiter &&
+create function  inserimento
+( measure varchar(25), unit varchar(25),_value int )
+returns json
+deterministic
+begin
+	declare a json; 
+    set a = concat('{
+									"values": [
+									{
+										"measure": "',measure,'",
+										"unit": "',unit,'",
+										"value": ',_value,'
+									}
+									]
+								}');
+	Return a;
+end &&
+delimiter ;
+DROP EVENT IF EXISTS voltage_event;
+DELIMITER $$
+CREATE EVENT voltage_event
+    ON SCHEDULE EVERY 1 SECOND
+        STARTS now()
+        ENDS now() + INTERVAL 40 SECOND
+    ON COMPLETION PRESERVE
+    DO BEGIN
+        DECLARE execution INT;
+        declare _timestamp timestamp;
+        set _timestamp = now();
+        SET execution = randnum(1,3);
+        IF
+            execution = 1
+		THEN
+            BEGIN
+                DECLARE voltage_value INT; -- value
+                DECLARE current_value INT;
+                DECLARE voltage_value1 INT; -- value
+                DECLARE current_value1 INT;-- valueS
+                declare exe2 int;
+                set exe2 = randnum(1,5);
+                SET voltage_value = randnum(0,10);
+                SET current_value = randnum(4,20);
+                SET voltage_value1 = randnum(0,10);
+                SET current_value1 = randnum(4,20);
+                if
+					exe2 = 1
+				then	
+					begin
+						INSERT INTO data_sensori
+						VALUES(
+                       _timestamp,
+                       inserimento( 'voltage', 'v', voltage_value1),
+                       1
+                      );
+                    end;
+				end if;
+                INSERT INTO data_sensori
+						VALUES(
+                       _timestamp,
+                       inserimento( 'voltage', 'v', voltage_value),
+                       1
+                      );
+            END;
+        END IF;
+
+
+    END $$
+DELIMITER ;
+
+
+set @door_value = null;
+
+
+																					-- tester
+
+drop procedure if exists tester;
+DELIMITER $$
+create procedure tester()
+begin
+DECLARE voltage_value INT;
+DECLARE _timestamp timestamp;
+set _timestamp = now();
+set voltage_value = 5;
+INSERT INTO data_sensori
+						VALUES(
+                       _timestamp,
+                       inserimento( 'voltage', 'v', voltage_value),
+						1
+                      );
+end $$
+DELIMITER ;
+
+
+
+
+
+delimiter ;
+
+
+DROP EVENT IF EXISTS sensor_2;
+
+DELIMITER $$
+CREATE EVENT IF NOT EXISTS sensor_2
+    ON SCHEDULE EVERY 1 SECOND
+        STARTS now()
+        ENDS now() + INTERVAL 40 SECOND
+    ON COMPLETION PRESERVE
+    DO BEGIN
+	
+		DECLARE execution INT;
+		SET execution = randnum(1,5);
+		IF
+				execution = 1
+		THEN 
+			BEGIN
+				set @door_value=coalesce(@door_value,randnum(0,1));
+				set @door_value = not @door_value;
+				call tester();
+				INSERT INTO data_sensori
+				VALUES(
+						  NOW(),
+						  CONCAT(' {
+										"values": [
+										  {
+											"measure": "open_closed",
+											"unit":"open",
+											"value": ',@door_value,'
+										  }
+										]
+									}'),
+
+						  2
+					  );
+		END;
+    END IF;
+END $$
+DELIMITER ;
+
+
+
+
+
+
+
+
+											
+	
+										-- procedure cleanup
+drop procedure if exists cleanup;
+DELIMITER $$
+CREATE PROCEDURE cleanup()
+BEGIN
+    DELETE FROM data_sensori;
+    DELETE FROM states;
+    DELETE FROM warnings;
+    drop event if exists voltage_event;
+    drop event if exists voltage_event;
+    SELECT * FROM data_sensori;
+    SELECT * FROM states;
+    SELECT * FROM warnings;
+END $$
+DELIMITER ;
+
+create table warnings(
+	timestamp datetime not null,
+    message varchar(255),
+    level enum('INFO','MEDIUM','HIGH','LUCACHAD')
+)
+											-- procedure warnings
+delimiter $$
+create procedure warn(
+	message varchar(255),
+    level enum('INFO','MEDIUM','HIGH','LUCACHAD')
+)
+begin 
+	insert into warnings values(now(),message,level);
+	
+end ;
+delimiter ;
+
+create table states(
+	chiave char(15) primary key,
+	valore char(75) 
+)engine memory;
+
+DELIMITER &&
+CREATE FUNCTION S2_LAST_VALUE_KEY()
+RETURNS CHAR(15)
+DETERMINISTIC
+BEGIN
+    RETURN 's2_last_value';
+END &&
+DELIMITER ;
+-- procedura che disabilita tutti gli eventi dei sensori
+DROP PROCEDURE IF EXISTS disable_all_events;
+DELIMITER $$
+CREATE PROCEDURE disable_all_events()
+BEGIN
+    ALTER EVENT voltage_event DISABLE;
+    ALTER EVENT sensor_2 DISABLE;
+END $$
+DELIMITER ;
+
+-- procedura che abilita tutti gli eventi dei sensori
+DROP PROCEDURE IF EXISTS enable_all_events;
+DELIMITER $$
+CREATE PROCEDURE enable_all_events()
+BEGIN
+DECLARE errno INT;
+    DECLARE msg TEXT;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1
+            errno = MYSQL_ERRNO,
+            msg = MESSAGE_TEXT;
+        CALL warn(CONCAT('ERRNO=',errno,', error=',msg),'HIGH');
+    END;
+    ALTER EVENT voltage_event ENABLE;
+    ALTER EVENT sensor_2 ENABLE;
+END $$
+DROP PROCEDURE IF EXISTS enable_events;
+
+DELIMITER $$
+CREATE PROCEDURE enable_events(
+    enable_sensor1 BOOL,
+    enable_sensor2 BOOL
+)
+BEGIN
+    IF
+        enable_sensor1
+    THEN
+        ALTER EVENT voltage_event ENABLE;
+    ELSE
+        ALTER EVENT voltage_event DISABLE;
+    END IF;
+    IF
+        enable_sensor2
+    THEN
+        ALTER EVENT sensor_2 ENABLE;
+    ELSE
+        ALTER EVENT sensor_2 DISABLE;
+    END IF;
+end $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS warn;
+
+DELIMITER $$ 
+CREATE PROCEDURE warn( 
+    message VARCHAR(255), 
+    level ENUM('INFO','MEDIUM','HIGH') 
+) 
+BEGIN
+    INSERT INTO warnings VALUES(NOW(),message,level);
+END $$
+DELIMITER ;
 -- ***************************************************************************************************************************
 -- *								FASE 2   SIMULAZIONE
 -- ***************************************************************************************************************************
 -- EVENTO SENSOR_1
+
 -- creare un evento che simula un sensore che produce dati di misurazione di corrente elettrica su un dispositivo
 -- il sensore produce due tipi di dato:
 	-- voltage con valori da 0 a 10 V
